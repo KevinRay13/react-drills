9ebcfa23ef378215b6440957fe954918b3011f95
Kevin Edmondson <kevinedmondson@Kevins-MacBook-Pro.local>
2019-01-11 16:01:26 -0600

    final

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      str : "",
+      rappers : 
+      ["Dr. Dre",
+       "Ice Cube",
+       "Snoop",
+       "Eazy E",
+       "Chance the Rapper"]
+    }
+    this.stringWrite = this.stringWrite.bind(this)
+  }
+  stringWrite(e){
+    this.setState({str: e.target.value})
+
+  }
+  
   render() {
+    let goats = this.state.rappers.filter((e) =>{
+      return e.includes(this.state.str)}).map((element, index) => {
+        return (
+          <h2 key = { index }>{ element }</h2>
+        )
+      })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" 
+        placeholder="type here"
+        onChange={(e) => this.stringWrite(e)}/>
+        { goats }
       </div>
     );
   }
