f38618759d37637fde21ac98470c4942f311c3c1
Kevin Edmondson <kevinedmondson@kevins-mbp.attlocal.net>
2019-01-12 13:44:45 -0600

    final

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      list: []
+    }
+    this.tasker = this.tasker.bind(this);
+  }
+
+  taskerChange( value ){
+    this.setState({input: value});
+  }
+  tasker(){
+    this.setState({
+      list: [...this.state.list, this.state.input],
+      input: ''
+
+    });
+  }
+  
   render() {
+    let list = this.state.list.map( (element, index) => {
+      return(
+        <Todo key={index} task={element}/>
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do List</h1>
+          <div>
+          <input 
+          value= {this.state.input}
+          onChange={(e) => this.taskerChange(e.target.value) }
+          placeholder="write tasks here"
+          />
+          <button onClick={this.tasker}
+          >add</button>
+          </div>
+          < br/>
+          {list}
       </div>
     );
   }
